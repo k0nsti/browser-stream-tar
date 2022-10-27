61522b23b201961d1eede99422345daddb4cadb1
Markus Felten <Markus.Felten@gmx.de>
2022-10-27 15:15:01 +0200

    test: fetch

---
+++ tests/entries-ava-browser.mjs
@@ -0,0 +1,17 @@
+import test from "ava";
+import { entries } from "browser-stream-tar";
+
+test("fetch entries", async t => {
+  const e = [{ name: "ABC" }];
+
+  const response = await fetch(
+    "https://github.com/k0nsti/browser-stream-tar/raw/main/tests/fixtures/test.tar"
+  );
+
+  let i = 0;
+  for await (const entry of entries(response.body)) {
+    t.is(entry.name, e[i].name);
+
+    i++;
+  }
+});
