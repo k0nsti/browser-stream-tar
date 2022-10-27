482b0d648568f6f4bbb30073e9a4462eac2448d9
Markus Felten <Markus.Felten@gmx.de>
2022-10-27 23:09:05 +0200

    test: make browser test work

---
+++ tests/assertions.mjs
@@ -0,0 +1,42 @@
+import { entries } from "browser-stream-tar";
+
+export async function assertTarStreamEntries(t, stream) {
+  const e = [
+    {
+      name: "a.txt",
+      size: 23,
+      content: new Uint8Array([
+        0x64, 0x61, 0x73, 0x20, 0x69, 0x73, 0x74, 0x20, 0x65, 0x69, 0x6e, 0x65,
+        0x20, 0x54, 0x65, 0x78, 0x74, 0x20, 0x44, 0x61, 0x74, 0x65, 0x69
+      ])
+    },
+    {
+      name: "b.csv",
+      size: 13,
+      content: new Uint8Array([
+        0x43, 0x20, 0x53, 0x20, 0x56, 0x20, 0x46, 0x20, 0x49, 0x20, 0x4c, 0x20,
+        0x45
+      ])
+    },
+    {
+      name: "z.doc",
+      size: 23,
+      content: new Uint8Array([
+        0x44, 0x69, 0x65, 0x20, 0x44, 0x6f, 0x6b, 0x75, 0x6d, 0x65, 0x6e, 0x74,
+        0x61, 0x74, 0x69, 0x6f, 0x6e, 0x20, 0x44, 0x61, 0x74, 0x65, 0x69
+      ])
+    }
+  ];
+
+  let i = 0;
+  for await (const entry of entries(stream)) {
+    t.is(entry.name, e[i].name, "name");
+    t.is(entry.size, e[i].size, "size");
+
+    const reader = entry.stream.getReader();
+    const { done, value } = await reader.read();
+    t.is(value.length, entry.size);
+    t.deepEqual(value, e[i].content);
+    i++;
+  }
+}
--- tests/entries-ava-browser.mjs
+++ tests/entries-ava-browser.mjs
@@ -1,17 +1,10 @@
 import test from "ava";
-import { entries } from "browser-stream-tar";
+import { assertTarStreamEntries } from "./assertions.mjs";
 
 test("fetch entries", async t => {
-  const e = [{ name: "ABC" }];
-
   const response = await fetch(
     "https://raw.githubusercontent.com/k0nsti/browser-stream-tar/main/tests/fixtures/test.tar"
   );
 
-  let i = 0;
-  for await (const entry of entries(response.body)) {
-    t.is(entry.name, e[i].name);
-
-    i++;
-  }
+  await assertTarStreamEntries(t, response.body);
 });
