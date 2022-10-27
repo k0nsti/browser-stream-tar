8bf2995d418c04c001cc5f2cfef04ac6c19378ff
Markus Felten <Markus.Felten@gmx.de>
2022-10-27 12:46:24 +0200

    docs: header

--- src/tar.mjs
+++ src/tar.mjs
@@ -1,5 +1,26 @@
 /**
  *
+ *    Field Name   Byte Offset     Length in Bytes Field Type
+ *  name         0               100             NUL-terminated if NUL fits
+ *  mode         100             8
+ *  uid          108             8
+ *  gid          116             8
+ *  size         124             12
+ *  mtime        136             12
+ *  chksum       148             8
+ *  typeflag     156             1               see below
+ *  linkname     157             100             NUL-terminated if NUL fits
+ *  magic        257             6               must be TMAGIC (NUL term.)
+ *  version      263             2               must be TVERSION
+ *  uname        265             32              NUL-terminated
+ *  gname        297             32              NUL-terminated
+ *  devmajor     329             8
+ *  devminor     337             8
+ *  prefix       345             155             NUL-terminated if NUL fits
+ */
+
+/**
+ *
  * @param {ReadableStream} tar
  * return AsyncIterator Object
  */
