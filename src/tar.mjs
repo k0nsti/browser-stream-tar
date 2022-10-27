74cc9d57322d49120dc7802cf915bc209f25c92e
Markus Felten <Markus.Felten@gmx.de>
2022-10-27 12:26:25 +0200

    feat: toString

--- src/tar.mjs
+++ src/tar.mjs
@@ -1,11 +1,21 @@
 /**
- * 
- * @param {ReadableStream} tar 
+ *
+ * @param {ReadableStream} tar
  * return AsyncIterator Object
  */
-export async function *entries(tar) {
-
+export async function* entries(tar) {
   const reader = tar.getReader();
-  let {done,value} = await reader.read();
+  let { done, value } = await reader.read();
 
+  const name = toString(value.subrray(0, 100));
+
+  yield { name };
+}
+
+function toString(bytes) {
+  const chars = [];
+  for (const i = 0; i < bytes.length; ) {
+    chars.push(((bytes[i++] & 0xff) << 8) | (bytes[i++] & 0xff));
+  }
+  return String.fromCharCode.apply(null, chars);
 }
