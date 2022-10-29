e35258b667187497af6a059e5d882548c0beda29
Markus Felten <Markus.Felten@gmx.de>
2022-10-29 14:19:26 +0200

    style: reformat

--- tests/util.mjs
+++ tests/util.mjs
@@ -1,39 +1,38 @@
-
-export async function readControlChunkSize(stream,chunkSize = 1 * 1024 * 1024) {
-
+export async function readControlChunkSize(
+  stream,
+  chunkSize = 1 * 1024 * 1024
+) {
   const reader = stream.getReader();
 
-let buffer;
+  let buffer;
 
-const readableWithDefinedChunks = new ReadableStream({
-  async pull(controller) {
-    let fulfilledChunkQuota = false;
+  return new ReadableStream({
+    async pull(controller) {
+      let fulfilledChunkQuota = false;
 
-    while (!fulfilledChunkQuota) {
-      const status = await reader.read();
+      while (!fulfilledChunkQuota) {
+        const status = await reader.read();
 
-      if (!status.done) {
-        const chunk = status.value;
+        if (!status.done) {
+          const chunk = status.value;
 
-        buffer = new Uint8Array([...(buffer || []), ...chunk]);
+          buffer = new Uint8Array([...(buffer || []), ...chunk]);
 
-        while (buffer.byteLength >= chunkSize) {
-          const chunkToSend = buffer.slice(0, chunkSize);
-          controller.enqueue(chunkToSend);
-          buffer = new Uint8Array([...buffer.slice(chunkSize)]);
+          while (buffer.byteLength >= chunkSize) {
+            const chunkToSend = buffer.slice(0, chunkSize);
+            controller.enqueue(chunkToSend);
+            buffer = new Uint8Array([...buffer.slice(chunkSize)]);
+            fulfilledChunkQuota = true;
+          }
+        }
+        if (status.done) {
           fulfilledChunkQuota = true;
+          if (buffer.byteLength > 0) {
+            controller.enqueue(buffer);
+          }
+          controller.close();
         }
-      }
-      if (status.done) {
-        fulfilledChunkQuota = true;
-        if (buffer.byteLength > 0) {
-          controller.enqueue(buffer);
-        }
-        controller.close();
       }
     }
-  },
-});
-
-return readableWithDefinedChunks
+  });
 }
