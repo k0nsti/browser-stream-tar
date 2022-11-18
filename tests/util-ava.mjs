7151e2e9c46b9110648e9194f9b4213a8a2e79e4
Markus Felten <Markus.Felten@gmx.de>
2022-11-18 19:44:06 +0000

    test: decode 0 length string

--- tests/util-ava.mjs
+++ tests/util-ava.mjs
@@ -9,6 +9,8 @@ import {
 
 test("decode string", t => {
   t.is(toString(new Uint8Array([65, 66, 67, 0])), "ABC");
+  t.is(toString(new Uint8Array([0])), "");
+  t.is(toString(new Uint8Array(0)), "");
 });
 
 test("decode integer", t => {
